82a2555287639d8abf8bfba4427974959ebf73a4
s3784876 <s3784876@student.rmit.edu.au>
2022-08-05 16:26:01 +1000

    Added required mod statements

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod traits;
+pub mod projections;
+pub mod two_dimensional;
+pub mod three_dimensional;
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
